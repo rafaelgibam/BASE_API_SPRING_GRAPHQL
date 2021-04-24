5c51472a73fece831ba0a56ce9a4788d0c324655
Rafael Willames <rafael.w.silva@hotmail.com>
2021-04-24 13:33:27 -0300

    feat: first commit

---
+++ src/main/java/br/com/baseapigraphql/BaseApiGraphqlApplication.java
@@ -0,0 +1,13 @@
+package br.com.baseapigraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseApiGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseApiGraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/baseapigraphql/BaseApiGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.baseapigraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseApiGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
